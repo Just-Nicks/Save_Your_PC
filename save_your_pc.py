e9d0b67edecc27fed33d884528f33e3b744156f9
Nicks <154464668+Just-Nicks@users.noreply.github.com>
2026-07-17 09:23:27 -0300

    Updated to v2
    Removed all functions except Download. Created one new function for all backups.

--- save_your_pc.py
+++ save_your_pc.py
@@ -12,161 +12,138 @@ themes = Theme({"error": "red bold", "success": "green", "warning": "yellow bold
 
 console = Console(theme=themes)
 
+obs_path = f"C:/Users/{username}/AppData/Roaming/obs-studio/basic"
+mc_path = f"C:/Users/{username}/AppData/Roaming/.minecraft"
+
+doc_path = f"C:/Users/{username}/Documents"
+pic_path = f"C:/Users/{username}/Pictures"
+video_path = f"C:/Users/{username}/Videos"
+
+
 def save_pc(disk):
     os.system('cls')
     final_path = f"{disk}/PC SAVED"
 
     if os.path.exists(f"{disk}/PC SAVED"):
-        console.print("[warning][!][/warning] A pasta de backups final ja existe!")
-        confirmation = Confirm.ask("[yellow][?][/yellow] Deseja continuar?")
+        console.print("[warning][!][/warning] The backup destination folder already exists!")
+        confirmation = Confirm.ask("[yellow][?][/yellow] Continue?")
 
         if confirmation:
-            console.print("[warning][!][/warning] Pasta antiga sendo deletada")
+            console.print("[warning][!][/warning] Deleting existing backup folder...")
             shutil.rmtree(f"{disk}/PC SAVED")
             os.makedirs(f"{disk}/PC SAVED")
 
-            save_obs(final_path)
-            save_mc(final_path)
-            save_files(final_path)
+            backup(
+                obs_path,
+                final_path,
+                "Obs Studio"
+            )
+            backup(
+                mc_path,
+                final_path,
+                f"Minecraft"
+            )
+            save_download(final_path)
+            backup(
+                doc_path,
+                final_path,
+                f"Documents"
+            )
+            backup(
+                pic_path,
+                final_path,
+                f"Pictures"
+            )
+            backup(
+                video_path,
+                final_path,
+                f"Videos"
+            )
 
         else:
-            console.print("[warning][!][/warning] Script foi abortado pelo usuario!")
+            console.print("[warning][!][/warning] Script aborted by the user!")
             time.sleep(3)
             return
     else:
         os.makedirs(f"{disk}/PC SAVED")
-        console.print("[success][+][/success] Diretorio criado com sucesso")
+        console.print("[success][+][/success] Directory created successfully!")
         time.sleep(3)
 
-        save_obs(final_path)
-        save_mc(final_path)
-        save_files(final_path)
+        backup(
+            obs_path,
+            final_path,
+            "Obs Studio"
+        )
+        backup(
+            mc_path,
+            final_path,
+            f"Minecraft"
+        )
+        save_download(final_path)
+        backup(
+            doc_path,
+            final_path,
+            f"Documents"
+        )
+        backup(
+            pic_path,
+            final_path,
+            f"Pictures"
+        )
+        backup(
+            video_path,
+            final_path,
+            f"Videos"
+        )
 
-def save_obs(final_path):
-    # OBS THEMES, PROFILES
+def backup(app_path, final_path, name):
+    # save_path = diretorio do save
+    # final_save = onde vai salvar
+    # name = nome do que vai ser salvo
     console.print("")
-    obs_path = f"C:/Users/{username}/AppData/Roaming/obs-studio/basic"
-
-    if not os.path.exists(obs_path):
-        console.print(f"[error][-][/error] O diretorio {obs_path} nao existe ou nao esta na pasta padrao!")
+    if not os.path.exists(app_path):
+        console.print(f"[error][-][/error] The directory {app_path} does not exist or is not located in the default path!")
         time.sleep(3)
-        return
+        return False
     else:
-        console.print(f"[success][+][/success] Diretorio {obs_path} foi encontrado. Copiando arquivos")
-        time.sleep(3)
+        console.print(f"[success][+][/success] Directory {app_path} found. Copying files...")
 
         try:
-            shutil.copytree(obs_path, f"{final_path}/obs studio")
-            console.print("[success][+][/success] O backup do OBS foi realizado com sucesso!")
-            time.sleep(5)
-
-        except Exception as e:
-            console.print(f"[error][-][/error] Ocorreu um erro ao realizar o backup: {e}")
-            time.sleep(5)
-
-def save_mc(final_path):
-    # MC FILES
-    console.print("")
-    mc_path = f"C:/Users/{username}/AppData/Roaming/.minecraft"
-
-    if not os.path.exists(mc_path):
-        console.print("[error][-][/error] O diretorio de saves/screenshots nao existe!")
-        time.sleep(3)
-        return
-    else:
-        console.print(f"[success][+][/success] Diretorio {mc_path} foi encontrado. Copiando arquivos")
-        time.sleep(3)
-
-        try:
-            shutil.copytree(f"{mc_path}/saves", f"{final_path}/minecraft/saves")
-            shutil.copytree(f"{mc_path}/screenshots", f"{final_path}/minecraft/screenshots")
-            
-            console.print("[success][+][/success] O backup dos SAVES e SCREENSHOTS de MINECRAFT foi realizado com sucesso!")
-            time.sleep(5)
-
-        except Exception as err:
-            console.print(f"[error][-][/error] Ocorreu um erro ao realizar o backup: {err}")
-            time.sleep(5)
-
-def save_files(final_path):
-    # DOCUMENTS
-    console.print("")
-    doc_path = f"C:/Users/{username}/Documents"
-
-    if not os.path.exists(doc_path):
-        console.print(f"[error][-][/error] O diretorio {doc_path} nao existe ou nao esta na pasta padrao!")
-        time.sleep(3)
-    else:
-        console.print(f"[success][+][/success] Diretorio {doc_path} foi encontrado. Copiando arquivos")
-        time.sleep(3)
-
-        try:
-            shutil.copytree(doc_path, f"{final_path}/Documents")
-            console.print("[success][+][/success] O backup de documentos foi realizado com sucesso!")
+            shutil.copytree(app_path, f"{final_path}/{name}")
+            console.print(f"[success][+][/success] The {name} backup was completed successfully!")
             time.sleep(3)
-
+            return True
         except Exception as err:
-            console.print(f"[error][-][/error] Ocorreu um erro ao realizar o backup: {err}")
-            time.sleep(5)
+            console.print(f"[error][-][/error] Failed to back up {name}: {err}")
+            time.sleep(3)
+            return False
 
+def save_download(final_path):
+    console.print("")
     # DOWNLOADS 
     download_path = f"C:/Users/{username}/Downloads"
 
     if not os.path.exists(download_path):
-        console.print(f"[error][-][/error] O diretorio {download_path} nao existe ou nao esta na pasta padrao!")
+        console.print(f"[error][-][/error] The directory {download_path} does not exist or is not located in the default path!")
         time.sleep(3)
     else:
-        console.print(f"[success][+][/success] Diretorio {download_path} foi encontrado.")
-        confirm_backup = Confirm.ask(f"[yellow][?][/yellow] Deseja copiar a pasta {download_path}?")
+        console.print(f"[success][+][/success] Directory {download_path} found. Copying files...")
+        confirm_backup = Confirm.ask(f"[yellow][?][/yellow] Do you want to copy the folder {download_path}?")
 
         if confirm_backup:
             try:
                 shutil.copytree(download_path, f"{final_path}/Downloads")
-                console.print("[success][+][/success] O backup de Downloads foi realizado com sucesso!")
+                console.print("[success][+][/success] The Downloads backup was completed successfully!")
                 time.sleep(3)
             except Exception as err:
-                console.print(f"[error][-][/error] Ocorreu um erro ao realizar o backup: {err}")
+                console.print(f"[error][-][/error] Failed to back up: {err}")
                 time.sleep(5)
-
-    # PICTUES 
-    pic_path = f"C:/Users/{username}/Pictures"
-
-    if not os.path.exists(pic_path):
-        console.print(f"[error][-][/error] O diretorio {pic_path} nao existe!")
-        time.sleep(5)
-    else:
-        console.print(f"[success][+][/success] O Diretorio {pic_path} foi encontrado. Iniciando backup!")
-
-        try:
-            shutil.copytree(pic_path, f"{final_path}/Pictures")
-            console.print("[success][+][/success] O backup de Pictures foi realizado com sucesso!")
-            time.sleep(3)
-        except Exception as err:
-            console.print(f"[error][-][/error] Ocorreu um erro ao realizar o backup: {err}")    
-            time.sleep(5)
-
-    # VIDEOS
-    video_path = f"C:/Users/{username}/Videos"
-
-    if not os.path.exists(video_path):
-        console.print(f"[error][-][/error] O diretorio {video_path} nao existe!")
-        time.sleep(5)
-    else:
-        console.print(f"[success][+][/success] O Diretorio {video_path} foi encontrado. Iniciando backup!")
-
-        try:
-            shutil.copytree(video_path, f"{final_path}/Videos")
-            console.print("[success][+][/success] O backup de Videos foi realizado com sucesso!")
-            time.sleep(3)
-        except Exception as err:
-            console.print(f"[error][-][/error] Ocorreu um erro ao realizar o backup: {err}")    
-            time.sleep(5)
 
 
 if __name__ == "__main__":
     while True:
         os.system('cls')
-        disk = Prompt.ask("[yellow][?][/yellow] Digite o nome do disco para backup EX: D:/")
+        disk = Prompt.ask("[yellow][?][/yellow] Enter the drive letter for the backup (e.g., D:/)")
 
         disk = disk.strip()
 
@@ -177,10 +154,10 @@ if __name__ == "__main__":
             disk += "/"
 
         if not os.path.exists(f"{disk}"):
-            console.print("[error][-][/error] O disco digitado nao existe!")
+            console.print("[error][-][/error] The drive you entered does not exist!")
             time.sleep(3)
             continue
         else:
-            console.print(f"[success][+][/success] Disco {disk} foi encontrado!")
+            console.print(f"[success][+][/success] Drive: ( {disk} ) was found!")
             save_pc(disk)
             break
